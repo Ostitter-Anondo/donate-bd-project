fce90f7815b4d5f4dab0a6284840ebb6d6fc13ca
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-09-24 22:55:03 +0600

    some more tweaking done for better visuals, font added

--- js/home.js
+++ js/home.js
@@ -5,16 +5,16 @@ document.getElementById("to-blog").addEventListener('click', function(){
 document.getElementById("pay-hst").addEventListener('click', function(){
   allHider("main-part", "dono-hst-box");
   document.getElementById("dono-btn").classList.add("btn-outline");
-  document.getElementById("dono-btn").classList.remove("bg-[#b4f461]");
+  document.getElementById("dono-btn").classList.remove("bg-[#b4f461]", "border-none");
   document.getElementById("pay-hst").classList.remove("btn-outline");
-  document.getElementById("pay-hst").classList.add("bg-[#b4f461]");
+  document.getElementById("pay-hst").classList.add("bg-[#b4f461]", "border-none");
 });
 document.getElementById("dono-btn").addEventListener('click', function(){
   allHider("main-part", "dono-cards");
   document.getElementById("pay-hst").classList.add("btn-outline");
-  document.getElementById("pay-hst").classList.remove("bg-[#b4f461]");
+  document.getElementById("pay-hst").classList.remove("bg-[#b4f461]", "border-none");
   document.getElementById("dono-btn").classList.remove("btn-outline");
-  document.getElementById("dono-btn").classList.add("bg-[#b4f461]");
+  document.getElementById("dono-btn").classList.add("bg-[#b4f461]", "border-none");
 });
 
 document.getElementById("btn-noakhali").addEventListener('click', function(event){
