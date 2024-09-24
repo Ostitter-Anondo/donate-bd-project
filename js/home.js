e5dbb649a0260cc2e3d53aff9c75e27afbfdd583
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-09-24 20:10:47 +0600

    backend done + transaction history

--- js/home.js
+++ js/home.js
@@ -12,3 +12,49 @@ document.getElementById("dono-btn").addEventListener('click', function(){
   document.getElementById("dono-btn").classList.remove("btn-outline");
   document.getElementById("dono-btn").classList.add("bg-[#b4f461]");
 });
+
+document.getElementById("btn-noakhali").addEventListener('click', function(event){
+  event.preventDefault();
+  const toAdd = Number(document.getElementById("amnt-noakhali").value);
+  let balance = Number(document.getElementById("balance").innerText);
+  let total = Number(document.getElementById("noakhali-total").innerText);
+  if (isNaN(toAdd) || toAdd <= 0 || toAdd > Number(balance)){
+    document.getElementById("failure-modal").showModal();
+    return false;
+  }
+  document.getElementById("balance").textContent = `${balance - toAdd}`;
+  document.getElementById("noakhali-total").textContent = `${total + toAdd}`;
+  trxMaker("Flood Relief at Noakhali, Bangladesh", toAdd);
+  document.getElementById("success-modal").showModal();
+  return true;
+})
+document.getElementById("btn-feni").addEventListener('click', function(event){
+  event.preventDefault();
+  const toAdd = Number(document.getElementById("amnt-feni").value);
+  let balance = Number(document.getElementById("balance").innerText);
+  let total = Number(document.getElementById("feni-total").innerText);
+  if (isNaN(toAdd) || toAdd <= 0 || toAdd > Number(balance)){
+    document.getElementById("failure-modal").showModal();
+    return false;
+  }
+  document.getElementById("balance").textContent = `${balance - toAdd}`;
+  document.getElementById("feni-total").textContent = `${total + toAdd}`;
+  trxMaker("Flood Relief at Feni, Bangladesh", toAdd);
+  document.getElementById("success-modal").showModal();
+  return true;
+})
+document.getElementById("btn-quota").addEventListener('click', function(event){
+  event.preventDefault();
+  const toAdd = Number(document.getElementById("amnt-quota").value);
+  let balance = Number(document.getElementById("balance").innerText);
+  let total = Number(document.getElementById("quota-total").innerText);
+  if (isNaN(toAdd) || toAdd <= 0 || toAdd > Number(balance)){
+    document.getElementById("failure-modal").showModal();
+    return false;
+  }
+  document.getElementById("balance").textContent = `${balance - toAdd}`;
+  document.getElementById("quota-total").textContent = `${total + toAdd}`;
+  trxMaker("Aid for Injured in the Quota Movement, Bangladesh", toAdd);
+  document.getElementById("success-modal").showModal();
+  return true;
+})
--- js/utilities.js
+++ js/utilities.js
@@ -13,30 +13,17 @@ function allHider(element, toUnhide = null){
   }
 }
 
-
-function timeFinder(){
-  const monthList = {
-    0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr',
-    4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug',
-    8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec',
-  };
-  const dayList = {
-    0: 'Sun', 1: "Mon", 2: "Tue", 3: "Wed", 4:"Thu", 5: "Fri", 6: "Sat"
-  };
-  const currentdate = new Date();
-  const fin = "Date: " + dayList[`${currentdate.getDay()}`] + monthList[`${currentdate.getMonth()}`] + " " +  currentdate.getDate() + " " + currentdate.getFullYear() + " " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds() + " GMT +0600 (Bangladesh Standard Time)";
-  return fin;
-}
-
 function trxMaker(location, amount){
   let div = document.createElement("div");
-  div.classList.add('flex', 'justify-around', 'w-9/12');
+  const rightNow = new Date();
+  div.classList.add('w-9/12', 'border', 'border-gray-300', 'rounded-lg', 'my-8', 'p-6');
   div.innerHTML = `
                   <h3 class="text-xl font-bold">
                     ${amount} Taka is Donated for ${location}
                   </h3>
+                  <br>
                   <p class="text-lg font-light">
-                    ${timeFinder()}
+                    ${rightNow.toString()}
                   </p>
   `;
   document.getElementById('trx-hst').appendChild(div);
